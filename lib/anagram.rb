f65e9534dbb5ead1bbf8866d36fcfe5e11d74557
jessica you <jessicaxytan@gmail.com>
2021-03-03 17:01:36 -0500

    commit tree b6a571db636035a986b7937b6b1a56d4b3c4f1ad

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,44 @@
 # Your code goes here!
+
+require 'pry'
+
+# class Anagram
+#     attr_accessor = :words
+
+#     def initialize(words)
+#         @words = words
+#     end
+
+#     # def broken_words
+#     #     @words.split("").sort
+#     # end
+
+#     # def broken_array(words)
+#     #     @words.split("").sort
+#     # end
+
+#     def match(array)
+#         array.select do |words|
+#             words.split("").sort == (array(words).split("")).sort
+#         end
+#     end
+   
+# end
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def broken_word
+        @word.split("").sort
+    end
+    def broken_array(word)
+        word.split("").sort
+    end
+    def match(array)
+       array.select do |word|
+        broken_array(word) == broken_word
+       end
+    end
+end
